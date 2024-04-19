6969a189a22a333793011be4e4fcfdc48e11971a
Zachary Proser <zackproser@gmail.com>
2024-04-19 09:34:13 -0400

    Install smart splits plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,12 +1,6 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- You can also add or configure plugins by creating files in this `plugins/` folder
--- Here are some examples:
-
 ---@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
+  { 'mrjones2014/smart-splits.nvim' },
 
   "andweeb/presence.nvim",
   {

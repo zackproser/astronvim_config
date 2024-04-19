f187609783008d0337b117e22280ba64d0850609
Zachary Proser <zackproser@gmail.com>
2024-04-19 09:42:35 -0400

    Smart splits working

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -46,10 +46,6 @@ return {
       n = {
         -- second key is the lefthand side of the map
 
-        -- navigate buffer tabs with `H` and `L`
-        L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
         -- mappings seen under group name "Buffer"
         ["<Leader>bD"] = {
           function()
